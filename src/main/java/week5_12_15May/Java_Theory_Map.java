8ab1f1dfde23e78192027a701c5d0b6d7df40f8f
MathuraveeraganeshMe <MathuraveeraganeshMe@10.0.75.1>
2020-06-05 21:30:07 +0530

    Java_Theory_Map
    1. What is Map? Differentiate between Hash, LinkedHash and Tree Map
    2. List the methods you know about the Map?

---
+++ src/main/java/week5_12_15May/Java_Theory_Map.java
@@ -0,0 +1,23 @@
+package week5_12_15May;
+/*
+ * 1. What is Map? Differentiate between Hash, LinkedHash and Tree Map
+2. List the methods you know about the Map?
+ */
+public class Java_Theory_Map {
+	
+	/*1. What is Map? Differentiate between Hash, LinkedHash and Tree Map
+		map is the combination of key with value,key won't allow duplicate but value allow duplicate
+		Hash is random order
+		LinkedHash is insertion order
+		Tree Map is ASCII order
+	2. List the methods you know about the Map?
+		Methods in Map Interface:
+		public Object put(Object key, Object value): This method is used to insert an entry in this map.
+		public void putAll(Map map): This method is used to insert the specified map in this map.
+		public Object remove(Object key): This method is used to delete an entry for the specified key.
+		public Object get(Object key):This method is used to return the value for the specified key.
+		public boolean containsKey(Object key): This method is used to search the specified key from this map.
+		public Set keySet(): This method is used to return the Set view containing all the keys.
+		public Set entrySet(): This method is used to return the Set view containing all the keys and values.
+	*/
+}	
